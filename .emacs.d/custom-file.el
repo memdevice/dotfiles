2ddeb08bc063b7f99e043ba0debd85f0edbd5f23
memdevice <58821778+memdevice@users.noreply.github.com>
2021-01-15 00:20:35 +0100

    Update custom-file.el

--- .emacs.d/custom-file.el
+++ .emacs.d/custom-file.el
@@ -5,9 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(leuven))
  '(tool-bar-mode nil))
-;;(custom-set-faces
+(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;;'(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
