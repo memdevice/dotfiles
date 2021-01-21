4cf82de1dc1d1477ff7e01fa908e0eb8ce8b6a66
LM <luca.missori@bancaditalia.it>
2021-01-21 13:14:02 +0100

    aggiunti F9 ed f12, electric pair, C-x k

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,10 @@
 ;; LM init file -- 20210115
 ;;
 
-;; LM: configurazioni fondamentali
+;; LM:________________________________________________________________
+;;
+;;     architettura del file system
+;; ___________________________________________________________________
 
 ;; niente backup file di emacs in giro,
 ;; meglio tutti da una parte (e non in ~/.emacs.d)
@@ -13,7 +16,14 @@
 (setq custom-file "~/.emacs.d/custom-file.el")
 (load-file custom-file)   ; necessario per essere sicuri che venga caricato...
 
-;; LM: interfaccia utente
+;; LM:________________________________________________________________
+;;
+;;     interfaccia utente
+;; ___________________________________________________________________
+
+;; se si preferisce partire con un file vuoto "untitled"
+;; invece che con lo scratch buffer (cfr. piu' sotto)
+;(setq initial-buffer-choice 'xah-new-empty-buffer)
 
 ;; Do not show the startup screen.
 (setq inhibit-startup-message t)
@@ -46,10 +56,14 @@
 
 ;; (setq  frame-title-format (format "%s's Emacs" (capitalize user-login-name)))
 ;; (setq frame-title-format (format " ――――――     %s's Emacs Emacs     ――――――――――― " (capitalize user-login-name))) ; standard
-(setq frame-title-format (format " ――――――  |    EscMetaAltControlShift  42   |  ―――――――――――――――――― ")) ; LM own (64 char)
+(setq frame-title-format (format " ――――――  |    EscMetaAltControlShift  42   |  ――――――――――――――――― ")) ; LM own (64 char)
 
 
-;;  ――――――――――――― LM: interazione utente i.e. "better default"  ――――――――――――――
+;; LM:________________________________________________________________
+;;
+;;     interazione utente i.e. "better default"
+;; ___________________________________________________________________
+
 
 ;; breviter
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -72,12 +86,10 @@
 
 (setq view-read-only t)   ; LM NB: il view-mode classico si attiva con C-c C-q
 
-;; Kill Other Buffers
-;; Here is a simple function that kills all buffers, except the current one.
-(defun kill-other-buffers ()
-  "Kill all other buffers."
-  (interactive)
-  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+;; auto close bracket insertion.
+;; typing any left bracket automatically insert the right matching bracket
+(electric-pair-mode 1)
+
 
 ;; ――――――――――――――――――― LM interazione con utente (completamento) ―――――――――――――
 
@@ -108,12 +120,18 @@
 ;; LM configurazioni di Emacs standard, senza installare pacakge
 ;; (emacs with batteeries included)
 
+;; Kill Other Buffers
+;; Here is a simple function that kills all buffers, except the current one.
+(defun kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+
 ;; recentf on --- keep a list of recently opened files
 (require 'recentf)
 (recentf-mode 1)
 (setq recentf-max-saved-items 256)
 (setq recentf-max-menu-items 42)
-
 
 ;; This calls another custom command of mine, revert-this-buffer. It does
 ;; exactly what the name implies: it reverts (reloads from file) the current
@@ -125,13 +143,49 @@
   (revert-buffer nil t t)
   (message (concat "Reverted buffer " (buffer-name))))
 
-;; ―――――――――――――――――――     LM keybind      ―――――――――――――――――――――――――――――
+;; Here's a command to open a new empty buffer, without prompting for a name.
+
+(defun xah-new-empty-buffer ()
+  "Create a new empty buffer.
+New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
+
+It returns the buffer (for elisp programing).
+
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+Version 2017-11-01"
+  (interactive)
+  (let (($buf (generate-new-buffer "untitled")))
+    (switch-to-buffer $buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)
+    $buf
+    ))
+
+;; reload emacs dofile (init.el) keybind
+;; It would be handy to have a global key binding to reload .emacs
+;; rather than go through the incredibly laborious process of
+;; M-x load-file (delete a long string if I'm deep into some directory)
+;;  ~/.emacs <RET>.
+(defun reload-dotemacs ()
+  (interactive)
+  (load-file "~/.emacs.d/init.el"))
+
+
+;; LM:________________________________________________________________
+;;
+;;     keybindings
+;; ___________________________________________________________________
+
+;;  F1 F2 F5 F6 F8 F9 F12
+;;
+;;  C-x k
 
 ;; better keybindings
 ;; cfr: https://www.masteringemacs.org/article/my-emacs-keybindings
 
 ;; <F1> M-x shell (basta C-h per l'help!)
-;; (global-set-key [f1] 'shell) ; F1
+(global-set-key [f1] 'shell) ; F1
+;; NB: ho configurato la bash di cygwin come inferior shell (M-x shell)
 
 ;; <F2> M-x rgrep (lo split windows si puo' fare con C-x 6)
 ;;(global-set-key [f2] 'xxxxxxxxx) ; F2
@@ -142,16 +196,28 @@
 ;; <F6> revert-this-buffer (reload from file)
 (global-set-key [f6] 'revert-this-buffer) ; F6
 
+;;(global-set-key [f7] 'xxxxxxxxx) ; F7
+
 ;; kill other buffer (elimina tutti tranne quello corrente)
 ;; forse sarebbe meglio usare un keybinding diverso (<F8>?)
 ;; (global-set-key (kbd "C-x C-b") 'kill-other-buffers)   ; C-x C-b
 (global-set-key [f8] 'kill-other-buffers) ; F8
 
+;; To open a new empty buffer, without prompting for a name
+(global-set-key [f9] 'xah-new-empty-buffer) ; F9
+
+;; reload emacs dofile (init.el) keybind
+(global-set-key [f12] 'reload-dotemacs)  ; F12
+
 ;; LMNB: "current" e non "this"!
 ;; cfr: https://stackoverflow.com/questions/6467002/how-to-kill-buffer-in-emacs-without-answering-confirmation
 (global-set-key (kbd "C-x k") 'kill-current-buffer)
 
-;; ―――――――――――――― LM custom-themes installed by hand  ―――――――――――――――――――――――――
+
+;; LM:________________________________________________________________
+;;
+;;     custom-themes installed by hand
+;; ___________________________________________________________________
 
 ;; aggiungo la direcotry dei temi alla variabile specifica
 ;;
@@ -159,7 +225,11 @@
 ;; https://www.emacswiki.org/emacs/CustomThemes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
-;; ――――――――――――――――― LM package installed by hand  ―――――――――――――――――――――――――
+;; LM:________________________________________________________________
+;;
+;;    package installed by hand
+;; ___________________________________________________________________
+
 
 ;; Tell emacs where is your personal elisp lib dir
 (add-to-list 'load-path "~/.emacs.d/lisp/")
@@ -181,7 +251,12 @@
 ;; (load "~/.emacs.d/myconfig")
 
 
-;;  UTF-8
+;; LM:________________________________________________________________
+;;
+;;     !cose sperimentali e rischiose!
+;; ___________________________________________________________________
+
+;;  ___________________ UTF-8 ________________________________________
 
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
@@ -199,3 +274,14 @@
 ;; make unix lineendings default
 (setq-default buffer-file-coding-system 'utf-8-unix)
 ;; Verify with C-h v buffer-file-coding-system. It should say "global value is utf-8-unix".
+
+;;  ___________________ cygwin shell _________________________________
+
+;; LM: usare la bash di cygwin
+;; da: https://www.masteringemacs.org/article/running-shells-in-emacs-overview
+;;
+(setq explicit-shell-file-name "c:/cygwin64/bin/bash.exe")
+(setq shell-file-name "bash")
+(setq explicit-bash.exe-args '("--noediting" "--login" "-i"))
+(setenv "SHELL" shell-file-name)
+(add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)
