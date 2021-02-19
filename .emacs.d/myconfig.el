eb0d65e8947f9b477527c802c2186a61c0497c75
LM <luca.missori@bancaditalia.it>
2021-02-19 15:57:00 +0100

    ripulisco da file eliminati sul repository

--- .emacs.d/myconfig.el
+++
@@ -1,6 +0,0 @@
-;; LM test rapido: configurazione standard vs. LM own
-
-;; Use your name in the frame title. :)
-;;
-;;(setq frame-title-format (format " ――――――     %s's Emacs Emacs     ――――――――――― " (capitalize user-login-name))) ; standard
-(setq frame-title-format (format " ――――――  |    EscMetaAltControlShift  42   |  ―――――――――――――――――― ")) ; LM own (64 char)
